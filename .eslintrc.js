b93d5a28b7b5892498f55f4661c7175ed5fb3344
Brian Andrews <sbardian@gmail.com>
2018-10-07 14:19:10 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
