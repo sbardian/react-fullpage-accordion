519ed3362dc0d9634461282a93876743bc54b02d
Brian Andrews <sbardian@gmail.com>
2018-10-07 14:54:46 -0600

    Component base setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-one-expression-per-line': 'off',
--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import 'normalize.css';
+import './save.css';
 
 class FullpageAccordion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      someState: 'test',
-    };
-  }
+  handleClick = e => {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+      if (panel !== this) {
+        panel.classList.remove('open');
+        panel.classList.remove('open-active');
+      }
+    });
+    e.target.classList.toggle('open');
+    e.target.classList.toggle('open-active');
+  };
 
   render() {
-    const { someState } = this.state;
+    const { data, height } = this.props;
+
     return (
-      <div>
-        <h1>hello ${someState}</h1>
+      <div className="panels">
+        {data.map(item => (
+          <div
+            type="button"
+            key={item.itemId}
+            className="panel"
+            style={{
+              backgroundImage: `url("${item.imageUrl}")`,
+              height: height || null,
+            }}
+            onClick={this.handleClick}
+            onKeyDown={this.handleClick}
+            role="button"
+            tabIndex={0}
+          >
+            <p>{item.top}</p>
+            <p>{item.middle}</p>
+            <p>
+              <a
+                style={{ zIndex: 20 }}
+                href="https://www.google.com"
+                alt={item.title}
+              >
+                {item.bottom}
+              </a>
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
