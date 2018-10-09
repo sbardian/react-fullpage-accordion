450f0ffcc03cb6133f520b5c190608d3bc4a1da8
Brian Andrews <sbardian@gmail.com>
2018-10-09 12:44:08 -0600

    Fix use of getByText

--- src/Panel.test.js
+++ src/Panel.test.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import { render, fireEvent, getByText } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import Panel from './Panel';
 import { dataSingle } from '../__mocks__/mockData';
 
 describe('Test Panel', async () => {
-  const handleClick = jest.fn();
-  const { container, debug } = render(
-    <Panel
-      item={dataSingle}
-      key={dataSingle.itemId}
-      onClick={handleClick}
-      activePanel="1"
-    >
-      <p>{dataSingle.top}</p>
-      <p>{dataSingle.middle}</p>
-      <p>
-        <a
-          style={{ zIndex: 20 }}
-          href="https://www.google.com"
-          alt={dataSingle.title}
-        >
-          {dataSingle.bottom}
-        </a>
-      </p>
-    </Panel>,
-  );
   // debug();
   it('Should find one panels div', () => {
-    const firstP = getByText(container, 'Hey');
-    const secondP = getByText(container, 'Lets');
-    const thirdP = getByText(container, 'Dance');
+    const handleClick = jest.fn();
+    const { container, debug, getByText } = render(
+      <Panel
+        item={dataSingle}
+        key={dataSingle.itemId}
+        onClick={handleClick}
+        activePanel="1"
+      >
+        <p>{dataSingle.top}</p>
+        <p>{dataSingle.middle}</p>
+        <p>
+          <a
+            style={{ zIndex: 20 }}
+            href="https://www.google.com"
+            alt={dataSingle.title}
+          >
+            {dataSingle.bottom}
+          </a>
+        </p>
+      </Panel>,
+    );
+    const firstP = getByText('Hey');
+    const secondP = getByText('Lets');
+    const thirdP = getByText('Dance');
     expect(firstP).toBeTruthy();
     expect(secondP).toBeTruthy();
     expect(thirdP).toBeTruthy();
