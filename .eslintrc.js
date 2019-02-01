4ec485a67af645c3b594ff2ca1a94ca925003b54
Brian Andrews <sbardian@gmail.com>
2019-02-01 15:56:26 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-one-expression-per-line': 'off',
+    eqeqeq: 'off',
   },
   plugins: ['jest'],
   env: {
