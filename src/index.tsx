daf7875ebda6667c4478fe91f825dd7a032c4f12
Brian Andrews <sbardian@gmail.com>
2019-02-04 10:38:02 -0700

    Add PropTypes to FullPageAccordion for none TS consumers

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/forbid-prop-types */
 import React, { CSSProperties, useState } from 'react';
+import PropTypes from 'prop-types';
 import 'normalize.css';
 import Panel from './Panel';
 import './styles.css';
@@ -53,6 +54,27 @@ const FullpageAccordion: React.FunctionComponent<FullpageAccordionProps> = ({
       ))}
     </div>
   );
+};
+
+FullpageAccordion.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      itemId: PropTypes.number.isRequired,
+      top: PropTypes.string.isRequired,
+      middle: PropTypes.string.isRequired,
+      bottom: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+  height: PropTypes.string,
+  textStyle: PropTypes.object,
+};
+
+FullpageAccordion.defaultProps = {
+  height: undefined,
+  textStyle: {},
 };
 
 export default FullpageAccordion;
